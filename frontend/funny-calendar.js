a85783e99cd1e0a8f8da3024554278601228bc04
kovacsborka <kovacsborbalaterez@gmail.com>
2022-03-03 17:07:52 +0100

    events added

--- frontend/funny-calendar.js
+++ frontend/funny-calendar.js
@@ -1,39 +1,65 @@
-function init(mathFunction){
+const formHTML = () => {
+    return `
+    <form>
+        <input name="input1" id="input1" type="text" />
+        <input name="input2" id="input2" type="text" />
+        <input name="input3" id="input3" type="text" />
+        <select name="animals" id="animals">
+            <option value="both5555">Both</option>
+            <option value="cats5555">Cats</option>
+            <option value="dogs5555">Dogs</option>
+        </select>
+        <button>Send</button>
+    </form>
+    `;
+};
 
-    var a = 5;
-    var b = 6;
+function loadEvent() {
+    console.log("load");
+    const rootElement = document.getElementById("root")
+
+    rootElement.insertAdjacentHTML("beforeend", formHTML())
+
+    //belementjük a formot egy változóba
+    const form = rootElement.querySelector("form")
+
+    //input esemény
+    const inputList = document.querySelectorAll("input")
+
+    /* 
+    for (const input of inputList) {
+        input.addEventListener("input", function(e) {                   ugyanaz mint a map pár sorral lejjebb
+            console.log(e.target.value);
+        })
+    }
+    }
+    */
+
+    Array.from(inputList).map(function(input){
+        input.addEventListener("input", function(e) {
+            console.log(e.target.value);
+        })
+    })
 
     
-    if (a < b){
-        let c = mathFunction(b, a);
-        /*         (function(){
-            var c = b - a;
-        })(); */
-        console.log(c);
-    } else {
-        let c = mathFunction(a, b);
-/*         (function(){
-            var c = a - b; 
-        })(); */
-        console.log(c);
-    };
-
-/* 
-    (function(){
-
-    })();
-
- */    
-};
-
-const initC = (firstNumber, secondNumber) => {
-    return firstNumber - secondNumber;
-};
-
-const initD = (firstNumber, secondNumber) => {
-    return firstNumber * secondNumber;
-};
+    //select esemény 
+    form.querySelector("select").addEventListener("input", function(e){
+        console.log(e.target.value);
+    })
 
 
-init(initD);
+    //sumbit esemény
+    form.addEventListener("submit", function(e){               //form a sor elején: már elmentettük egy változóba
+        e.preventDefault()
+        console.log(e.target);
+    })
+    
 
+}
+window.addEventListener("load", loadEvent);
+
+
+
+
+//formok esetében nem click event hanem submit event
+//sumbit eseményt NEM a gombra tesszük hanem a formra
